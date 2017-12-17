bf27b2de62cb239cc108c5769a24f12b083cfa4a
Naresh Chennuri <chennurinaresh9@gmail.com>
2017-12-17 07:51:40 +0530

    Adding theming and css

--- src/app/book.ts
+++ src/app/book.ts
@@ -4,4 +4,5 @@ export class Book {
   author: string;
   publicationDate: string;
   cover: string;
+  numberOfPages: number;
 }
--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -1,4 +1,4 @@
-import { Book } from './book';
+import {Book} from './book';
 
 export const BOOKS: Book[] = [
   {
@@ -7,20 +7,23 @@ export const BOOKS: Book[] = [
     author: 'C.T.WENTE',
     publicationDate: 'Dec 31, 1969',
     cover: 'https://images-na.ssl-images-amazon.com/images/I/51XKTTP-SkL._SY346_.jpg',
+    numberOfPages: 298
   },
   {
     id: 2,
     title: 'Annie\'s Verdict',
     author: 'John Ellsworth',
     publicationDate: 'August 3, 2017',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg',
+    numberOfPages: 987
   },
   {
     id: 3,
     title: 'Eat, Pray, Die ',
     author: ' Chelsea Field',
     publicationDate: 'July 3, 2016',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg',
+    numberOfPages: 803
   },
   {
     id: 4,
@@ -28,20 +31,23 @@ export const BOOKS: Book[] = [
     author: 'C.T.WENTE',
     publicationDate: 'Dec 31, 1969',
     cover: 'https://images-na.ssl-images-amazon.com/images/I/51XKTTP-SkL._SY346_.jpg',
+    numberOfPages: 668
   },
   {
     id: 5,
     title: '5Annie\'s Verdict',
     author: 'John Ellsworth',
     publicationDate: 'August 3, 2017',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg',
+    numberOfPages: 598
   },
   {
     id: 6,
     title: '6Eat, Pray, Die ',
     author: ' Chelsea Field',
     publicationDate: 'July 3, 2016',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg',
+    numberOfPages: 358
   },
   {
     id: 7,
@@ -49,20 +55,23 @@ export const BOOKS: Book[] = [
     author: 'C.T.WENTE',
     publicationDate: 'Dec 31, 1969',
     cover: 'https://images-na.ssl-images-amazon.com/images/I/51XKTTP-SkL._SY346_.jpg',
+    numberOfPages: 321
   },
   {
     id: 8,
     title: '8Annie\'s Verdict',
     author: 'John Ellsworth',
     publicationDate: 'August 3, 2017',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg',
+    numberOfPages: 794
   },
   {
     id: 9,
     title: '9Eat, Pray, Die ',
     author: ' Chelsea Field',
     publicationDate: 'July 3, 2016',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg',
+    numberOfPages: 965
   },
   {
     id: 10,
@@ -70,26 +79,30 @@ export const BOOKS: Book[] = [
     author: 'C.T.WENTE',
     publicationDate: 'Dec 31, 1969',
     cover: 'https://images-na.ssl-images-amazon.com/images/I/51XKTTP-SkL._SY346_.jpg',
+    numberOfPages: 478
   },
   {
     id: 11,
     title: '11Annie\'s Verdict',
     author: 'John Ellsworth',
     publicationDate: 'August 3, 2017',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51Eh8qOSJlL.jpg',
+    numberOfPages: 974
   },
   {
     id: 12,
     title: '12Eat, Pray, Die ',
     author: ' Chelsea Field',
     publicationDate: 'July 3, 2016',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg',
+    numberOfPages: 346
   },
   {
     id: 13,
     title: '13Eat, Pray, Die ',
     author: ' Chelsea Field',
     publicationDate: 'July 3, 2016',
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51qcNAv2o6L._SY346_.jpg',
+    numberOfPages: 279
   },
 ];
